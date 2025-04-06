cc5968e97a784ae7e60d16931194feadf7599f99
Phoenix Nova <thephoenixnova@gmail.com>
2025-04-06 17:00:56 +0700

    First commit

---
+++ meduimimagezoom.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import ProductImage from './ProductImage';
+
+// const productData = {
+//   imageUrl: 'path/to/your/product.jpg',
+//   altText: 'Product Name'
+// };
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <ProductImage {...productData} />
+//     </div>
+//   );
+// }
+
+// export default App;
